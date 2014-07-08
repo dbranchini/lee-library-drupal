0ae1c8c590e04fb93d53ed75222b51a8d7e63ffc
Darcy Branchini <dad284@cornell.edu>
2014-07-08 13:34:52 -0400

    Fixed 'return to top' button behavior.
    It appeared too soon when scrolling and over the top of the login link.

--- js/return-to-top.js
+++ js/return-to-top.js
@@ -2,7 +2,7 @@
 
 jQuery(function( $ ){
   $(window).scroll(function() {
-      if ($(this).scrollTop() >= 200) {        // If page is scrolled more than 50px
+      if ($(this).scrollTop() >= 300) {        // If page is scrolled more than 50px
           $('#return-to-top').fadeIn(200);    // Fade in the arrow
       } else {
           $('#return-to-top').fadeOut(200);   // Else fade out the arrow
